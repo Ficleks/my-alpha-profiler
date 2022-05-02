5282b203289ba7f1e39806e77edc876539260af3
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-05-02 00:50:06 -0300

    update: fetch habilitado

--- src/pages/services/api.jsx
+++ src/pages/services/api.jsx
@@ -46,7 +46,7 @@ export const getSession = async () => {
         .then(resp => resp.text());
 }
 
-export const editProfile = async (password, name, email, birthday, image) => {
+/* export const editProfile = async (password, name, email, birthday, image) => {
     return api.put("/user/edit",
         {
             name: name,
@@ -54,34 +54,34 @@ export const editProfile = async (password, name, email, birthday, image) => {
             email: email,
             birthday: birthday,
             photo: image
-        }
+        },  { withCredentials: true }
     );
+} */
+
+export const editProfile = async (password, name, email, birthday, image) => {
+
+    const userData =
+    {
+        name: name,
+        password: password,
+        email: email,
+        birthday: birthday,
+        photo: image
+    }
+
+    const requestOptions = {
+
+        method: 'PUT',
+        headers: {
+        Accept: 'application/json',
+             'Content-Type': 'application/json',
+             'Access-Control-Allow-Origin': 'http://localhost:3000',
+             'Access-Control-Allow-Credentials': true
+         },
+         credentials: 'include',
+         body: JSON.stringify(userData)
+     };
+
+     return await fetch(apiUrl + '/user/edit', requestOptions)
+         .then(resp => resp.text());
 }
-
-// export const editProfile = async (password, name, email, birthday, image) => {
-
-//     const userData =
-//     {
-//         name: name,
-//         password: password,
-//         email: email,
-//         birthday: birthday,
-//         photo: image
-//     }
-
-//     const requestOptions = {
-
-//         method: 'PUT',
-//         headers: {
-//             Accept: 'application/json',
-//             'Content-Type': 'application/json',
-//             'Access-Control-Allow-Origin': 'http://localhost:3000',
-//             'Access-Control-Allow-Credentials': true
-//         },
-//         credentials: 'include',
-//         body: JSON.stringify(userData)
-//     };
-
-//     return await fetch(apiUrl + '/user/edit', requestOptions)
-//         .then(resp => resp.text());
-// }
