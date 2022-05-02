0b2dd95e2b42771bedc34d43bb745c3b262cf4d0
ayoline <ayo@outlook.com.br>
2022-05-02 13:03:47 -0300

    add some css do edit profile

--- src/pages/EditProfilePage/index.jsx
+++ src/pages/EditProfilePage/index.jsx
@@ -37,68 +37,73 @@ const EditProfilePage = () => {
         navigate("/");
     }
 
+    const handleDeleteClick = (e) => {
+        console.log('entrou delete')
+    }
+
     return (
-        <div className="container">
-            <div className="header">
-                <h1>Editar Perfil</h1>
-                <Button onClick={handleBackHomeClick}>Voltar</Button>
-            </div>
-            <div className="profile-data">
-                <form className="form" onSubmit={handleSubmit}>
-                    <div className="field">
-                        <label htmlFor="name">Nome</label>
-                        <input
-                            type="text"
-                            name="name"
-                            id="name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
+        <div className="edit-container-body">
+            <div className="edit-container">
+                <div className="edit-header">
+                    <button className="edit-delete-btn" onClick={handleDeleteClick}>Deletar</button>
+                    <h2>Editar Perfil</h2>
+                    <Button onClick={handleBackHomeClick}>Voltar</Button>
+                </div>
+                <div className="edit-profile-data">
+                    <form className="form" onSubmit={handleSubmit}>
+                        <div className="field">
+                            <label htmlFor="name">Nome</label>
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="birthday">Data de Nascimento</label>
+                            <input
+                                type="date"
+                                name="birthday"
+                                id="birthday"
+                                value={birthday}
+                                onChange={(e) => setBirthday(e.target.value)}
+                            />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="password">Senha</label>
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <div className="edit-actions">
+                            <Button type="submit">Salvar</Button>
+                        </div>
+                    </form>
+                    <div className="edit-image">
+                        <img
+                            alt="avatar"
+                            className="my-img"
+                            src="https://www.pngall.com/wp-content/uploads/12/Avatar-PNG-Image.png"
                         />
+                        <input type="file" />
                     </div>
-                    <div className="field">
-                        <label htmlFor="birthday">Data de Nascimento</label>
-                        <input
-                            type="date"
-                            name="birthday"
-                            id="birthday"
-                            value={birthday}
-                            onChange={(e) => setBirthday(e.target.value)}
-                        />
-                    </div>
-                    <div className="field">
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div className="field">
-                        <label htmlFor="password">Senha</label>
-                        <input
-                            type="password"
-                            name="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <div className="field">
-                        <label htmlFor="image">Imagem</label>
-                        <input
-                            type="text"
-                            name="image"
-                            id="image"
-                            value={image}
-                            onChange={(e) => setImage(e.target.value)}
-                        />
-                    </div>
-                    <div className="actions">
-                        <Button type="submit">Salvar</Button>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
     );
